e14546c1088a07fbf5a24e774502dd4ec2c1b49d
rocha06101 <ferpa.felipe2@gmail.com>
2026-01-20 22:18:31 -0300

    Subindo tela home concluída

--- almirante-tamandare-frontend/src/app/pages/home/home.ts
+++ almirante-tamandare-frontend/src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { SectionsComponent } from '../../shared/components/sections/sections';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, SectionsComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
